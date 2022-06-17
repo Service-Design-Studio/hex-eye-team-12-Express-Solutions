9c8e16ea1a30aec4e7c593c1622919e3ea0c1be9
jefflai0315 <jefflai0315@gmail.com>
2022-06-17 18:09:14 +0800

    added topservices and main categories section

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_14_082958) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_16_151833) do
   create_table "services", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.text "requirements"
     t.datetime "ttc"
     t.string "service_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "transactions_trackers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
