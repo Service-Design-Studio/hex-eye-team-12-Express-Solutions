f99a681a622ac28b79dea91ce86978e4128503a8
pinkadotted <subkmrspm@gmail.com>
2022-06-21 23:05:05 +0800

    Co-authored-by: Jeff Lai <Jefflai0315@users.noreply.github.com> Co-authored-by: ioitami <ioitami@users.noreply.github.com> Co-authored-by: brutatoasta <brutatoasta@users.noreply.github.com> Co-authored-by: spizzray <spizzray@users.noreply.github.com>

---
+++ features/step_definitions/web_steps.rb
@@ -0,0 +1,23 @@
+require 'uri'
+require 'cgi'
+# require File.expand_path(File.join(File.dirname(FILE), "..", "support", "paths"))
+# require File.expand_path(File.join(File.dirname(FILE), "..", "support", "selectors"))
+
+module WithinHelpers
+  def with_scope(locator)
+    locator ? within(*selector_for(locator)) { yield } : yield
+  end
+end
+World(WithinHelpers)
+
+Given /^(?:|I )am on (.+)$/ do |page_name|
+    visit '/services'
+  end
+
+# When /^(?:|I )click on the "([^"]*)"$/ do |button|
+#   click_button(button)
+# end
+
+When /I click on the "(.*)"/ do |locator|
+  page.click_link locator
+end
