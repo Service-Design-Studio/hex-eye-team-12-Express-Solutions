b7e7cd6d7408d27495a3089c769aab33e16cfd77
jefflai0315 <jefflai0315@gmail.com>
2022-06-22 00:31:57 +0800

    added new route time_estimate

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # Custom routes
   # url for services, with a id in parameters, to: controller
   get '/services/:id/sub', to:'services#sub', as: 'sub_service'
-  
+  get '/service/:id/time_estimate', to:'service#time_estimate', as: 'time_estimate'
 
   root :to => redirect('/services')
 
