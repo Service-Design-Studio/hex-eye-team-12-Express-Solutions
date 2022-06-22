a41cad2b551df2b86cbc69fdbb49b47fb0d2a87f
Io <jowie_ng@mymail.sutd.edu.sg>
2022-06-22 14:42:44 +0800

    commit

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,7 +31,6 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  config.include Capybara::DSL
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
