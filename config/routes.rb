33b12d0c4916be017e3f2f29717d72e880a0f928
Io <jowie_ng@mymail.sutd.edu.sg>
2022-06-23 02:27:36 +0800

    Textbox full link w. Rails

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,7 @@ Rails.application.routes.draw do
   # url for services, with a id in parameters, to: controller
   get '/services/:id/sub', to:'services#sub', as: 'sub_services'
   get '/services/:id/time_estimate', to:'services#time_estimate', as: 'time_estimate'
-<<<<<<< Updated upstream
   get '/more', to:'services#more', as: 'more_services'
-=======
->>>>>>> Stashed changes
 
   root :to => redirect('/services')
 
