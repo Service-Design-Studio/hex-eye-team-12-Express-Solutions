0050112c608509c34983c77dc23d3727c351817a
pinkadotted <subkmrspm@gmail.com>
2022-06-22 20:02:29 +0800

    deleted comments

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,6 @@ Rails.application.routes.draw do
   get '/services/:id/sub', to:'services#sub', as: 'sub_services'
   get '/services/:id/time_estimate', to:'services#time_estimate', as: 'time_estimate'
   get '/more', to:'services#more', as: 'more_services'
-  # #Set default first page as /home
-  # get '/services', to:'services#index', as: 'index'
 
   root :to => redirect('/services')
 
