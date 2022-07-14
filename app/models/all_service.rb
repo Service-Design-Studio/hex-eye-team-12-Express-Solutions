84a7a697ba0d66310bcbf05fd53306f13352a0a6
Raymond Harrison <spizzray@gmail.com>
2022-07-14 13:17:55 +0800

    finished cucumber change topic.csv to topics.csv put back distinct in all service model removed @variables at index html more.feature added seed.services and test QR for all platform time estimate changed background from category to services. sms step implement QR step
    Co-authored-by: pinkadotted <pinkadotted@users.noreply.github.com>
    Co-authored-by: YeoKawaii <yeokewei@users.noreply.github.com>
    Co-authored-by: brutatoasta <brutatoasta@users.noreply.github.com>

--- app/models/all_service.rb
+++ app/models/all_service.rb
@@ -1,7 +1,7 @@
 class AllService < ActiveRecord::Base
     def self.all_category 
         #find unique categories
-        return AllService.select(:category)
+        return AllService.select(:category).distinct[1..-1]
     end
 
     def self.top_services
