156ae5c05d7448baee9c66e405111d4f7c24f461
yeokewei <yeokewei@gmail.com>
2022-07-27 21:02:33 +0800

    Revert "Merge branch 'main' of https://github.com/Service-Design-Studio/final-project-hex-eye-team-12"
    This reverts commit 869d4cc125a61d046642591548cb40f836103d8d, reversing
    changes made to 3426888aef5efafefc7305743b8dc23195d0ebe9.

--- db/migrate/20220726122156_add_description_to_all_service.rb
+++
@@ -1,5 +0,0 @@
-class AddDescriptionToAllService < ActiveRecord::Migration[7.0]
-  def change
-    add_column :all_services, :description, :string
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,24 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+# more_services = [
+#   {:name => 'test_name', :description => 'test_description', :requirements => 'test_requirements', :ttc => 'test_ttc', :service_id => 'test_service_id'}
+# ]
+
+# more_services.each do |service|
+#   Service.create!(service)
+# end
 
 require 'csv'
 
 service_table = CSV.read("db/clean_txn.csv")
-service_table.each do |category, category_id,service_id, service, digital_time, branch_time, migratable, count, details, 
-                        service_image_link, service_alt_text, cat_image_link, cat_alt_text, description, deep_link|
-    AllService.create!( :category => category,
-                        :category_id => category_id, 
-                        :service_id => service_id, 
-                        :service => service, 
-                        :migratable => migratable, 
-                        :count => count, 
-                        :details => details, 
-                        :digital_time => digital_time, 
-                        :branch_time => branch_time,
-                        :service_image_link => service_image_link,
-                        :service_alt_text => service_alt_text,
-                        :cat_image_link => cat_image_link,
-                        :cat_alt_text => cat_alt_text,
-                        :description => description,
-                        :deep_link => deep_link
-                        )
+service_table.each do |category, category_int, service, migratable, count, details, digital_time, branch_time|
+    AllService.create!(:category => category, :category_int => category_int, :service => service, :migratable => migratable, :count => count, :details => details, :digital_time => digital_time, :branch_time => branch_time)
 end
 
 bank_details_table = CSV.read("db/sms_numbers.csv")
@@ -32,9 +29,4 @@ end
 topics_table = CSV.read("db/topics.csv")
 topics_table.each do |topic|
     Topic.create!(:topic => topic)
-end
-
-duration_table = CSV.read("db/branch_time.csv")
-duration_table.each do |branch, id, wait_time|
-    Duration.create!(:branch => branch, :branch_id => id, :wait_time => wait_time)
 end
