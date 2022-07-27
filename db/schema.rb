94b1319eb3c002e49d4e9119a1bc2fb735840038
yeokewei <yeokewei@gmail.com>
2022-07-27 18:00:44 +0800

    Added deeplink column to db
    Migrated and seeded "deeplink"
    Removed Dummy_services

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_26_122156) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_27_094916) do
   create_table "all_services", force: :cascade do |t|
     t.string "category"
     t.integer "category_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_26_122156) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
+    t.string "deep_link"
   end
 
   create_table "branches", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ require 'csv'
 
 service_table = CSV.read("db/clean_txn.csv")
 service_table.each do |category, category_id,service_id, service, digital_time, branch_time, migratable, count, details, 
-                        service_image_link, service_alt_text, cat_image_link, cat_alt_text, description|
+                        service_image_link, service_alt_text, cat_image_link, cat_alt_text, description, deep_link|
     AllService.create!( :category => category,
                         :category_id => category_id, 
                         :service_id => service_id, 
@@ -19,7 +19,8 @@ service_table.each do |category, category_id,service_id, service, digital_time, 
                         :service_alt_text => service_alt_text,
                         :cat_image_link => cat_image_link,
                         :cat_alt_text => cat_alt_text,
-                        :description => description
+                        :description => description,
+                        :deep_link => deep_link
                         )
 end
 
