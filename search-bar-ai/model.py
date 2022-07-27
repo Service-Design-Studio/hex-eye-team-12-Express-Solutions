393b211606fa97dbfd7753383ff330abdcd79a23
Jeff Lai <79297365+Jefflai0315@users.noreply.github.com>
2022-07-28 02:50:41 +0800

    add treshold to ml- 0.2

--- search-bar-ai/model.py
+++ search-bar-ai/model.py
@@ -11,8 +11,22 @@ def search(query):
     passage_embedding = model.encode(desc)
     result = util.dot_score(query_embedding, passage_embedding)
     result_array = result.numpy()
-    top_5_index =result_array.argsort()[-1][::-1][:5]
-    top_5_index = top_5_index.tolist()
+    order_result = result.sort()
+    top_5_index = order_result[1].tolist()[0][::-1]
+    top_5_result = order_result[0].tolist()[0][::-1]
+    print(top_5_index)
+    print(top_5_result)
+    for i in range(len(top_5_result)):
+        # print(top_5_index[i])
+        print(top_5_result[i])
+        if top_5_result[i] <0.2:
+            top_5_result = top_5_result[0:i]
+            break
+            
+    top_5_index= top_5_index[0:len(top_5_result)]
+    print(top_5_result)
+    # top_5_index =result_array.argsort()[-1][::-1][:5]
+    # top_5_index = top_5_index.tolist()
     print(top_5_index)
     output={'top_5_index': top_5_index,
     'top_5_services': table.iloc[top_5_index]['Service'].tolist()} 
