4e5e0242575b2f2d633bc60197b6f5ba329bdcd1
pinkadotted <subkmrspm@gmail.com>
2022-08-02 17:52:58 +0800

    added code into all_service.rb and services_controller.rb
    Co-authored-by: YeoKawaii <yeokewei@users.noreply.github.com>

--- app/models/all_service.rb
+++ app/models/all_service.rb
@@ -6,8 +6,12 @@ class AllService < ActiveRecord::Base
         return AllService.order(:category_id).select(:category , :category_id, :cat_image_link).distinct[1..-1]
     end
 
-    def self.top_services
-        return AllService.all.sort_by(&:count).reverse[0..2]
+    def self.top_services(today, branch)
+        # return AllService.all.sort_by(&:count).reverse[0..2]
+        # getting the list of top 5 services from the csv file based on the date and branch
+       return PredictedService.select(today, branch).sort_by(&:predicted_services).reverse[0..4]
+
+
     end
 
     def self.services
@@ -17,5 +21,7 @@ class AllService < ActiveRecord::Base
         end
         return services
     end
+
+
 
 end
