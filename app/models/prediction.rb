a959b00c2856ae766138bdaf21096a232e7290df
yeokewei <yeokewei@gmail.com>
2022-08-08 23:28:06 +0800

    "Account closure" as suggested srv is removed
    Edited prediction.rb to remove and add the next suggusted service
    Also amended the default suggested services (removed account closure)
    Amended Cucumber to work with changes described

--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -12,20 +12,32 @@ class Prediction < ActiveRecord::Base
         avail_dates = Prediction.distinct.pluck(:unixdate)[1..]
 
         if avail_dates.include? ndate #if dates can be found in db
-            if branch != nil #if got branch
-                return Prediction.where(unixdate: ndate, branch_name: branch).order(prediction: :desc).pluck(:service).first(number)
+            if branch != nil #Assigns var if branch exists
+                find_branch = branch
             else
-                return Prediction.where(unixdate: ndate, branch_name: 'Global').order(prediction: :desc).pluck(:service).first(number)
+                find_branch = 'Global'
             end
+            # Return top n services for branch
+            predict_arr = Prediction.where(unixdate: ndate, branch_name: find_branch).order(prediction: :desc).pluck(:service).first(number)
+
+             #check if "Account Closure" Exists: if true, replace with the (n+1)th suggested
+            if predict_arr.include? 'ACCOUNT CLOSURE':
+                predict_arr = predict_arr - ['ACCOUNT CLOSURE'] +  [Prediction.offset(number).where(unixdate: ndate, branch_name: find_branch).order(prediction: :desc).pluck(:service).first]
+            end
+            
+            return predict_arr #array
+
         else
             #if dates cannot be found in db
             #return a the top 5 services based on DBS transaction data (Clean_txn.csv)
-            return ['UPDATE CUSTOMER PARTICULARS',
+            default_arr = ['UPDATE CUSTOMER PARTICULARS',
                 'UPDATE CUSTOMER PHONE',
-                'ACCOUNT CLOSURE',
+                'FUND TRANSFER',
                 'DEBIT CARD ISSUANCE',
                 'DEBIT CARD REPLACEMENT'
                 ]
+
+            return default_arr[..number-1]
         end
     end
 
